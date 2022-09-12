2e76219c9847c32c5d49fc1a7f66282891468b01
Fenobi <13667739122@qq.com>
2022-09-12 21:55:51 +0800

    添加项目文件。

---
+++ fu.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
+	int i = 12;
+	double j = 1.223;
+	std::cout << i <<std::endl<< j << std::endl;
+	return 0;
+}
